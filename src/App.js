a57bf30f724b4e851fe88c9f59830bb8195c340f
kanekyo1234 <kanekyohunter.0314@softbank.ne.jp>
2020-07-19 00:25:53 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <p>とりあえず作っといた！！</p>
+      <p>ghjk</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import App from "./App";
+import 'bulma/css/bulma.css'
+
+render(<App />, document.querySelector("#content"));
