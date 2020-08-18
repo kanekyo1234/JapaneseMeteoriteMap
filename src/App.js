5406fd36e8ce90547ffc2b818f2ac2efb0ebcd20
yk-hts <chyk18074@g.nihon-u.ac.jp>
2020-08-18 17:42:11 +0900

    devide with color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-// import React from "react";
-
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-// import * as d3 from "d3";
-// import * as topojson from "topojson";
 
 import { ChoroplethMapPage } from "./ChoroplethMap";
 
