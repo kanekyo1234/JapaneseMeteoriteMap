37f3b3b66a01872e7277a790fc58511ae8a0a3c0
kanekyo1234 <kanekyohunter.0314@softbank.ne.jp>
2020-08-18 23:11:10 +0900

    miss

--- src/ChoroplethMap.js
+++ src/ChoroplethMap.js
@@ -6,9 +6,12 @@ import Jsondata from "./output.json";
 const ChoroplethMap = ({ features }) => {
   const width = 1060;
   const height = 700;
-
+  // var browserWidth = $(window).width();
   const datas = Jsondata;
+  let now = [];
+  now.push("10");
 
+  const [Era, setEra] = useState(); //年代
   const projection = d3.geoMercator().scale(2000).center([129.69167, 40.68944]);
   const path = d3.geoPath().projection(projection);
   const color = d3
@@ -54,30 +57,8 @@ const ChoroplethMap = ({ features }) => {
     }
   }
   return (
-
     <body>
-      <div class="field">
-        <div class="control">
-          <div class="select is-primary">
-            <select>
-              <option>dropdown</option>
-              <option>With options</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      <div class="field">
-        <div class="control">
-          <div class="select is-primary">
-            <select>
-              <option>Select dropdown</option>
-              <option>With options</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      <p>dfghjkl</p>
-
+      {/* <EraBox></EraBox> */}
       <svg width={width} height={height} style={stroke}>
         <g>
           {features.map((feature, i) => (
@@ -88,18 +69,123 @@ const ChoroplethMap = ({ features }) => {
             const x = projection([data.経度, data.緯度])[0];
             const y = projection([data.経度, data.緯度])[1];
             console.log(datas.length);
-            return (
-              <circle
-                cx={x}
-                cy={y}
-                r={calcR(data["総重量 (kg)"])}
-                fill={getColor(data)}
-              />
-            );
+            console.log(now[0]);
+            console.log(calcR(data["総重量 (kg)"]));
+            if (now[0] === calcR(data["総重量 (kg)"])) {
+              return (
+                <circle
+                  cx={x}
+                  cy={y}
+                  r={calcR(data["総重量 (kg)"])}
+                  fill={getColor(data)}
+                />
+              );
+            }
           })}
         </g>
       </svg>
     </body>
+  );
+};
+
+const Erabox = (props) => {
+  const change = (event) => {
+    console.log(event.target.value);
+    props.setI(event.target.value - 1);
+  };
+  return (
+    <form>
+      <div class="select">
+        <select onChange={change}>
+          <option value="0">----</option>
+          <option value="1"></option>
+          <option value="2">電子政府率の国際比較</option>
+          <option value="3">行政のオンライン化状況</option>
+          <option value="4">マイナンバーカード普及率</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+
+// const Setumei = () => {
+//   return (
+//     <body>
+//       <div>
+//         <article class="message is-dark">
+//           <div className="container">
+//             <section className="section">
+//               <div class="message-header">
+//                 <p>隕石ページの描画</p>
+//               </div>
+//               <div class="message-body">
+//                 過去861年から現在に至るまでの日本に落ちた隕石の位置を描画している.隕石一つ一つのデータがjsonファイルに格納されており,内訳は
+//                 <strong>
+//                   名前,落下場所,年月日,種類,総重量 (kg),個数,緯度,経度
+//                 </strong>
+//                 である.その中で使用するデータは
+//                 <div class="content">
+//                   <ol type="1">
+//                     <li>年</li>
+//                     <li>総重量</li>
+//                     <li>緯度</li>
+//                     <li>経度</li>
+//                   </ol>
+//                 </div>
+//                 の四つである.それぞれどのように反映させているかというと,年のデータでまとまった年代ごとの色分けを行う.緯度,経度の数値を日本地図上に反映し,隕石が落ちた場所を円の中心とするようにする.日本地図がすでに実際の緯度経度が反映されて描かれているので位置は正確に描画される.そして円の大きさを総重量の値によって決める.
+//               </div>
+//             </section>
+//           </div>
+//         </article>
+
+//         <article class="message is-link">
+//           <div class="message-header">
+//             <p>可視化の目的</p>
+//           </div>
+//           <div class="message-body">この可視化は日本のどこに隕石が落ち</div>
+//         </article>
+//       </div>
+//       <div class="field">
+//         <div class="control">
+//           <div class="select is-primary">
+//             <select>
+//               <option>dropdown</option>
+//               <option>With options</option>
+//             </select>
+//           </div>
+//         </div>
+//       </div>
+//       <div class="field">
+//         <div class="control">
+//           <div class="select is-primary">
+//             <select>
+//               <option>Select dropdown</option>
+//               <option>With options</option>
+//             </select>
+//           </div>
+//         </div>
+//       </div>
+//     </body>
+//   );
+// };
+
+const Select = (props) => {
+  const change = (event) => {
+    console.log(event.target.value);
+    props.setI(event.target.value - 1);
+  };
+  return (
+    <form>
+      <div class="select">
+        <select onChange={change}>
+          <option value="0">----</option>
+          <option value="1">インターネット普及率</option>
+          <option value="2">電子政府率の国際比較</option>
+          <option value="3">行政のオンライン化状況</option>
+          <option value="4">マイナンバーカード普及率</option>
+        </select>
+      </div>
+    </form>
   );
 };
 
@@ -118,7 +204,6 @@ export const ChoroplethMapPage = () => {
   }
   return <ChoroplethMap features={features} />;
 };
-
 
 // import React, { useEffect, useState } from "react";
 // import * as d3 from "d3";
