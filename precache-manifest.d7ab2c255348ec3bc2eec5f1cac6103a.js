b1dbb4ec8c5fe2ef9c6c2273a7de688ebf65c4a2
kanekyo1234 <kanekyohunter.0314@softbank.ne.jp>
2020-08-19 06:15:43 +0900

    Updates

--- precache-manifest.d7ab2c255348ec3bc2eec5f1cac6103a.js
+++ precache-manifest.d7ab2c255348ec3bc2eec5f1cac6103a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f4d08926a87fd42439a81c550c8cfead",
+    "revision": "4c59ffcfe7e6cb6328ce0ff55838485b",
     "url": "/SVG-tutorial/index.html"
   },
   {
-    "revision": "aa2f63552cdbdd873796",
+    "revision": "8f0b7b34859bfe4b8365",
     "url": "/SVG-tutorial/static/css/2.0c0672e5.chunk.css"
   },
   {
-    "revision": "aa2f63552cdbdd873796",
-    "url": "/SVG-tutorial/static/js/2.dfdbaa12.chunk.js"
+    "revision": "8f0b7b34859bfe4b8365",
+    "url": "/SVG-tutorial/static/js/2.fe8ce235.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/SVG-tutorial/static/js/2.dfdbaa12.chunk.js.LICENSE.txt"
+    "url": "/SVG-tutorial/static/js/2.fe8ce235.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4fdb160e1aa394b17494",
-    "url": "/SVG-tutorial/static/js/main.15ceb64e.chunk.js"
+    "revision": "6a564de0feaaea62086e",
+    "url": "/SVG-tutorial/static/js/main.401e57cd.chunk.js"
   },
   {
     "revision": "8a7d0627cdbadb929706",
